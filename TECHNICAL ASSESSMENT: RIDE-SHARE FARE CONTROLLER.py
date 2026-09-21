3af206fda09ada0317956ccbd96f7ca13c08f6f6
itsloni <ayoolaoluwalonimi15@gmail.com>
2026-09-21 06:43:16 +0100

    CLEAN Code for RIDE-SHARE controller build

--- TECHNICAL ASSESSMENT: RIDE-SHARE FARE CONTROLLER.py
+++ TECHNICAL ASSESSMENT: RIDE-SHARE FARE CONTROLLER.py
@@ -10,6 +10,8 @@
 
 active_trip = False
 active_surge = False
+picked_passenger = False
+waited_for_passenger = False
 surge_price = 0
 base_fares = 20
 
@@ -20,78 +22,90 @@ while True:
             if not active_trip:
                 print("Driver enroute")
                 active_trip = True
-                # while True:
-                #     surge_cost = input("Enter your surge cost if applicable: ")
-                if not active_surge:
-                    surge_cost = input("I sthere currently a surge? enter (Y)/(N): ")
-                    if surge_cost == "Y":
-                        # surge_cost = int(surge_cost)
-                        surge_price += 10
-                        print("Surge cost locked in.")
-                        active_surge = True
-
-                    elif surge_cost == "N":
-                        print("alright no surge.")
-                        active_surge = False
-                    else:
-                        print("Please enter a valid response, (Y)/(N).")
+                while True:
+                    surge_cost = input("Is there currently a surge? enter (Y)/(N): ").lower()
+                    if not active_surge:
+                        if surge_cost == "y":
+                            surge_price += 10
+                            print("Surge cost locked in.")
+                            active_surge = True
+                            break
+                        elif surge_cost == "n":
+                            print("alright no surge.")
+                            active_surge = False
+                            break
+                        else:
+                            print("Please enter a valid response, (Y)/(N).")
             else:
                 print("Driver already enroute.")
 
         elif status == "end" or status == "drop-off":
-            if active_trip:
+            if active_trip and picked_passenger:
                 if active_surge:
-                # if not active_trip:
                     total_fares = base_fares + surge_price
                     print(f"Your ride has ended and your Total price is ${total_fares}")
                     break
-                    # else:
-                    #     print("You can't end/drop-off. You can only cancel.")
                 else:
                     total_fares = base_fares
                     print(f"Your ride has ended and your Total price is ${total_fares}")
                     break
-            elif not active_trip:
+            else:
                 print("You can't end/drop-off. You can only cancel.")
+
     elif status == "waiting":
         if active_trip:
-            # if active_surge:
-            print("Waiting for passenger")
-            active_trip = False
-            # active_surge = False
-            # else:
-            #     print("Already Waiting for passenger")
+            if not waited_for_passenger:
+                print("Waiting for passenger")
+                active_trip = True
+                waited_for_passenger = True
+            else:
+                print("Already Waiting for passenger or picked up passenger already. Please confirm and try again.")
         else:
             print(" Car never stopped to wait, or is already waiting for passenger.")
+
     elif status == "continue" or status == "pickup":
-        if not active_trip and not active_surge:
-            fares += 20
-            print("Driver has picked passenger and continued")
-            active_surge = True
+        if active_trip and waited_for_passenger == True:
+            if picked_passenger == False:
+                print("Driver has picked passenger and continued")
+                # active_trip = True
+                picked_passenger = True
+            else:
+                print("Driver has already picked the passenger. Pls confirm and try again.")
         else:
             print("Driver never waited or Driver has already picked the passenger. Pls confirm and try again.")
 
     elif status == "cancel":
         if active_trip:
-            print("Cancelling ride")
-            fares = 0
-            print(f"Your ride has ended and your Total price is ${fares}")
-            active_trip = False
-            active_surge = True
-            break
+            if picked_passenger == True:
+                driver_response = input("""
+Are you sure?, You currently have a passenger with you.
+be sure to know that you will loose your money!")(Y)/(N): 
+""").lower()
+                if driver_response == "y":
+                    fares = 0
+                    print("Cancelling ride.")
+                    print(f"Your ride has ended and your Total price is ${fares}")
+                    active_trip = False
+                    break
+                elif driver_response == "n":
+                    print("Alright you Just go on with the ride, when you reach destination you end.")
+                else:
+                    print("Please enter invalid response.")
+            else:
+                driver_response = input("Beware,You haven't gotten a passenger. Are you sure? (Y)/(N): ").lower()
+                if driver_response == "y":
+                    print("Cancelling ride.")
+                    fares = 0
+                    print(f"Your ride has ended and your Total price is ${fares}")
+                    active_trip = False
+                    break
+                elif driver_response == "n":
+                    print("Alright you Just go on with the ride, when you reach destination you end.")
+                else:
+                    print("Please enter invalid response.")
         else:
             print("Already Canceled.")
 
-    # elif status == "end" or status == "drop-off":
-    #     if active_trip and not active_surge:
-    #         if not active_trip:
-    #             total_fares = fares + surge_price
-    #             print(f"Your ride has ended and your Total price is ${total_fares}")
-    #             break
-    #         else:
-    #             print("You can't end/drop-off. You can only cancel.")
-    #     else:
-    #         print("You can't end/drop-off. You can only cancel.")
     else:
         print("Invalid input")
 
